eed6b6bf8d6b8078b6fe31428f7a1cbb8d3b4ba2
Vinicius T. Dias <viniciustdias2001@gmail.com>
2021-04-25 19:01:44 -0300

    Item Pedido and ItemPedidoPk

--- src/main/java/com/viniciusDias/cursomc/CursomcApplication.java
+++ src/main/java/com/viniciusDias/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.viniciusDias.cursomc.domain.Cidade;
 import com.viniciusDias.cursomc.domain.Cliente;
 import com.viniciusDias.cursomc.domain.Endereco;
 import com.viniciusDias.cursomc.domain.Estado;
+import com.viniciusDias.cursomc.domain.ItemPedido;
 import com.viniciusDias.cursomc.domain.Pagamento;
 import com.viniciusDias.cursomc.domain.PagamentoComBoleto;
 import com.viniciusDias.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.viniciusDias.cursomc.repositories.CidadeRepository;
 import com.viniciusDias.cursomc.repositories.ClienteRepository;
 import com.viniciusDias.cursomc.repositories.EnderecoRepository;
 import com.viniciusDias.cursomc.repositories.EstadoRepository;
+import com.viniciusDias.cursomc.repositories.ItemPedidoRepository;
 import com.viniciusDias.cursomc.repositories.PagamentoRepository;
 import com.viniciusDias.cursomc.repositories.PedidoRepository;
 import com.viniciusDias.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -120,6 +125,18 @@ public class CursomcApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
--- src/main/java/com/viniciusDias/cursomc/domain/Produto.java
+++ src/main/java/com/viniciusDias/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.viniciusDias.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -32,7 +35,28 @@ public class Produto implements Serializable {
     		inverseJoinColumns = @JoinColumn(name = "categoria_id")
     )
     private List<Categoria> categorias = new ArrayList<>();
+    
+    @OneToMany(mappedBy="id.produto")
+    private Set<ItemPedido> itens = new HashSet<>();
 
+    public Produto() {
+	}
+	
+	public Produto(Integer id, String nome, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -64,15 +88,13 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
-
-	public Produto() {
-	}
 	
-	public Produto(Integer id, String nome, Double preco) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.preco = preco;
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
@@ -99,8 +121,4 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-
-
 }
